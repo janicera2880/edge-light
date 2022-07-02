384875abee6d63bbb0733fef35a29bed4d50895c
Janice Alecha <janiceralecha@gmail.com>
2022-07-01 19:49:23 -0500

    Modify codes in index.css db.json index.html index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 });
 //  Create a function for dropdown menu using get elements by ID
@@ -12,8 +12,20 @@ function myNav() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
+  
+  
+      fetch('http://localhost:3000/lights')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // LOG: (3) [{…}, {…}, {…}]
+      });
+    
+  
+
+
   // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
+  document.onclick = function(e) {
     if (!e.target.matches('.dropbtn')) {
     let myDropdown = document.getElementById("myDropdown");
       if (myDropdown.classList.contains('show')) {
@@ -21,6 +33,10 @@ function myNav() {
       }
     }
   }
+ 
+  
+  
+
 //Add Event Listener to Email Button and console.log message Your Message Is Sent
 document.getElementById("myBtn").addEventListener("click", myFunction);
 
