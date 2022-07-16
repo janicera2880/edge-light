315df5765a04a7272980b9fdac843019a8c0f26b
Janice <janiceralecha@gmail.com>
2022-07-15 20:38:37 -0500

    Add background color to css for mouseover event

--- js/index.js
+++ js/index.js
@@ -26,11 +26,13 @@ function renderedObject(light){
 let gallery = document.createElement('li')
 gallery.className ='grid-item'
 gallery.innerHTML = 
-`<img src=${light.image} width="300" height="400"/>
-<div class="content">
+`<img src=${light.image} width="400" height="500"/>
 <h5>${light.name}</h5>
+<div class="content">
+
 </div>
 <div class="description">
+  <h5>${light.name}</h5>
   <p>Description:</p>
   <p>${light.category}</p>  
   <p>${light.style}</p>
@@ -76,67 +78,6 @@ console.log('after get all lights')
 }
 initialize()
 // Create Elements for footer and create submit event
-
-function queryForm()
-{
-    let queryBox = document.getElementById("queryBox").style.display = "block";
-    let queryForm = document.getElementById("queryForm");
-    let linebreak = document.createElement("br");
-
-    let lblName = document.createElement("label");
-    lblName.textContent = "Name: ";
-    queryForm.appendChild(lblName);
-
-    let fullName = document.createElement("input");
-    fullName.name = "fullName";
-    fullName.type = "text";
-    fullName.required = "required";
-    queryForm.appendChild(fullName);
-    queryForm.appendChild(linebreak);
-
-
-    let lblEmail = document.createElement("label");
-    lblEmail.textContent = "Email: ";
-    queryForm.appendChild(linebreak);
-    queryForm.appendChild(lblEmail);
-
-    let email = document.createElement("input");
-    email.name = "email";
-    email.type = "text";
-    email.required = "required";
-    queryForm.appendChild(email);
-
-
-    let lblPhoneNumber = document.createElement("label");
-    lblPhoneNumber.textContent = "Phone Number: ";
-    queryForm.appendChild(linebreak);
-    queryForm.appendChild(lblPhoneNumber);
-
-    let phoneNumber = document.createElement("input");
-    phoneNumber.name = "phoneNumber";
-    phoneNumber.type = "text";
-    phoneNumber.required = "required";
-    queryForm.appendChild(phoneNumber);
-
-
-    let submitQuery = document.createElement("input");
-    submitQuery.type = "submit";
-    submitQuery.value = "Submit Query";
-    queryForm.appendChild(linebreak);
-    queryForm.appendChild(submitQuery);
-
-    let resetForm = document.createElement("input");
-    resetForm.type = "reset";
-    resetForm.value = "Reset";
-    queryForm.appendChild(resetForm);
-
-    document.getElementById("#myFormId").addEventListener("submit", function(e){
-      if(!isValid){
-          e.preventDefault();    
-      }
-      
-    });
-  }
 
 
 
