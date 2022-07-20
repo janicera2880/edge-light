cd1efe10b65c44165d0c2c0bf053c033d6cd3095
Janice <janiceralecha@gmail.com>
2022-07-19 21:01:42 -0500

    Modify and add event listener favorite button to each gallery picture

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  //The DOMContentLoaded event is fired when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-  //Declare variable form by ID product-form
+  console.log('The DOM has loaded');
+  
+  //I wanted to create a demo search button where you can just type in any keyword and will remove search words when x is clicked
   let form = document.getElementById('product-form')
   form.addEventListener('submit', function(event){
     event.preventDefault()
@@ -9,26 +10,28 @@ document.addEventListener('DOMContentLoaded', () => {
     button = document.createElement('button')
     button.textContent = ' x '
     button.addEventListener('click', function(){
-      this.parentNode.remove()
+    this.parentNode.remove()
     })
+
     p.textContent = `${event.target['mySearch'].value} `
     console.log(p)
     form.appendChild(p)
     p.appendChild(button)
+
+   
   })
+
 });
 
-//
+//I wanted to build a gallery of products in grid view to my main page light fixture catalog
 function renderedObject(light){
 //Fetch URL and Build Gallery of Lights
 let gallery = document.createElement('li')
 gallery.className ='grid-item'
 gallery.innerHTML = 
 `<img src=${light.image} width="400" height="500"/>
-<h5>${light.name}</h5>
-<div class="content">
-
-</div>
+<h5>${light.name}</h5><div class="content">
+<button id="button">Favorite❤️</button>
 <div class="description">
   
   <p>Description:</p>
@@ -39,11 +42,22 @@ gallery.innerHTML =
   <p>${light.size}</p>
   <p>${light.material}</p>
   <p>${light.voltage}</p>
-
+  <p>${light.glass.glass_material}</p>
+  <p>${light.glass.glass_color}</p>
 </div>`
-
-//Add Gallery to the DOM
 document.querySelector('#light-list').appendChild(gallery)
+
+
+gallery.addEventListener('click', () => {
+  const button = gallery.querySelector('#button');
+
+  button.innerText = 'Added To Favorite❤️';
+  
+setTimeout(() => {
+  button.innerText = 'Favorite❤️';
+  }, 1000);
+});
+
 // Create Mouseover Event and Mouseleave
 gallery.addEventListener("mouseover", event => {
  //console.log("mouse in")
@@ -52,7 +66,7 @@ gallery.addEventListener("mouseover", event => {
  descriptionDiv.classList.add("displayed")
 });
 gallery.addEventListener("mouseleave", event => {
-  //console.log("mouse leave")
+  //console.log("mouse out")
   const descriptionDiv = gallery.querySelector(".description")
   
   descriptionDiv.classList.remove("displayed")
@@ -60,25 +74,25 @@ gallery.addEventListener("mouseleave", event => {
 
 }
 
+
 //Fetch Request- Get Fetch for All Lights
-function getAllLights(){
+function getAllLights() {
 fetch('http://localhost:3000/lights')
 .then(res => res.json())
-.then(lights => lights.forEach(light => console.log(light)))
-//.then(lights => console.log(lights))
-//my second .then gives me my entire data array which is "lights". Next, for each light object in my array I'm passing each one to my renderObject function
-//.then in a GET request will always give you your entire data object
-//.then(lights => lights.forEach(light => renderedObject(light)))
-//gallery.array.forEach(element => console.log(light));
-  
+.then(lights => lights.forEach(light => renderedObject(light)))
 
-console.log() 
+//.then(lights => lights.forEach(light => console.log(light)))
+
+//.then(lights => console.log(lights))
+
+//my second .then gives me my entire data array which is "lights". Next, for each light object in my array I'm passing each one to my renderObject function
+//.then in a GET request will always display entire data object
 }
 
 function initialize(){
-//lightData.array.forEach(light => renderedObject(light))
+
 getAllLights()
-console.log('after get all light')
+
 
 }
 initialize()
@@ -89,12 +103,11 @@ initialize()
 //The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
 //Clicking submit button will prevent it fropm submitting a form
 for (let clickButton of
-  document.getElementsByClassName("clickbutton"))
+  document.getElementsByClassName("clickButton"))
   clickButton.addEventListener("click", alertMessage);
   function alertMessage() {
-alert("Your Email Was Successfully Sent, We'll Get Back With You Shortly!");
-e.preventDefault()
-}
+     alert("Your Email Was Successfully Sent, We'll Get Back With You Shortly!");
+  }
 
 
 
